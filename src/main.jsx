f6c806d1c7a0af9cff2e3e988dad181fdf19f2d4
swanith1234 <swanithpidugu@gmail.com>
2024-11-30 16:41:05 +0530

    Add folder: src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App userData={{"name":"winner","emailId":"pidugubunny534@gmail.com","phoneNo":"winner","projects":[{"title":"winner","description":"winner","technologies":["React"],"repoLink":"winner","_id":"674af227662bdf5fd1eebfb7"}],"resume":"","experiences":[{"companyName":"winner","role":"winner","description":"winner","technologiesUsed":[],"duration":"winner","_id":"674af227662bdf5fd1eebfb8"}],"techStacks":[],"contactDetailsUrls":[],"codingProfilesUrls":[],"certificationsUrls":[],"achievements":[{"title":"winner","description":"winner","_id":"674af227662bdf5fd1eebfb9"}],"about":"winner","profilePhoto":"","_id":"674af227662bdf5fd1eebfb6","createdAt":"2024-11-30T11:08:23.551Z","updatedAt":"2024-11-30T11:08:23.551Z","__v":0}} />
+  </StrictMode>,
+)
